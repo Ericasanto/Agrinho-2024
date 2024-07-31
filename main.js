bcf844271c9ceecd25de5936503495c60ced09b4
Erica_Marco <126908056+Ericasanto@users.noreply.github.com>
2024-07-31 11:45:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -92,3 +92,29 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+var modal = document.getElementById('myModal');
+
+
+var modalImg = document.getElementById('modalImage');
+
+var imgs = document.querySelectorAll('.destino-imagem, .atividade-imagem');
+
+imgs.forEach(img => {
+    img.onclick = function() {
+        modal.style.display = 'block';
+        modalImg.src = this.src;
+    }
+});
+
+var span = document.getElementsByClassName('close')[0];
+
+span.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
